e05f4ab916d8711db282b75f7bc6d25858f8eae4
Elisabeth Kummer <elisabeth.kummer@tptlive.ee>
2025-09-04 10:54:12 +0300

    num

--- index.php
+++ index.php
@@ -1,62 +1,25 @@
 <?php
 
-$array = array(1, 2, 3);
-var_dump($array);
-$array = [1, 2, 3];
-var_dump($array);
-$array = [
-    1, 
-    'Liisu',
-    true,
-    [1, 2, 3], 
-    3.4, 
-        ];
-var_dump($array);
-$array = [
-    100 => 1,
-    2,
-    'name' => 'Liisu', 
-    'age' => 18,
-    'isFemale' => true,
-    3,
-    4,
-    5,
-        ];
-var_dump($array);
-$array = [
-    1,
-    2,
-    'name' => 'Liisu', 
-    'age' => 18,
-    'isFemale' => true,
-    1534 => 3,
-    4,
-    5,
-        ];
-var_dump($array);
-
-$array = [1, 2, 3];
-var_dump($array[1]);
-
-$array = [
-    'name' => 'Liisu', 
-    'age' => 18,
-    'isFemale' => true,
-        ];
-var_dump($array['age']);
-
-$array = [
-    [1, 2, 3], 
-    [4, 5, 6], 
-    [7, 8, 9], 
-];
-var_dump($array[2][1]);
-
-$array = [1, 2, 3];
-array_push($array, 4);
-array_push($array, 5, 6, 7);
-$array[] = 8;
-$array[1] = 'something else';
-var_dump($array);
-
-
+$num = 1;
+$num = $num + 1;
+var_dump($num);
+$num = $num - 1;
+var_dump($num);
+$num = $num * 2;
+var_dump($num);
+$num = $num / 2;
+var_dump($num);
+$num = $num % 2;
+var_dump($num);
+$num += 1;
+$num -= 1;
+$num *= 1;
+$num /= 1;
+$num %= 1;
+var_dump($num);
+$num++;
+$num--;
+var_dump($num);
+$num = $num ** 2;
+$num **= 2;
+var_dump($num);
