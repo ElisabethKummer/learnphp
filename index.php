c25f01fc050e1d40eee88970d5f646bd2de55140
Elisabeth Kummer <elisabeth.kummer@tptlive.ee>
2025-09-04 10:15:03 +0300

    Algne

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+echo "Hello World!\n";
+echo "Hello World!\n";
+$name = 'Liisu';
+echo 'hello' . $name;
+echo "\n";
+$age = 18;
+echo 'Your name is ' . $name . ' and you are ' . $age . ' years old!';
+echo "\n";
+echo 'Your name is $name and you are $age year sold!!';
+echo "\n";
+echo "Your name is $name and you are $age year sold!!";
+echo "\n";
+
+$var = 1;
+var_dump($var);
+$var = 1.4;
+var_dump($var);
+$var = "Hello";
+var_dump($var);
+$var = "Hellõ";
+var_dump($var);
+$var = "😭";
+var_dump($var);
+$var = true;
+var_dump($var);
+$var = false;
+var_dump($var);
+$bar = null;
+var_dump($bar);
+
+?>
