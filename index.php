2be9da3b7c6f98ff899e57c7bfb6732e9a251445
Elisabeth Kummer <elisabeth.kummer@tptlive.ee>
2025-09-04 10:42:35 +0300

    array

--- index.php
+++ index.php
@@ -1,33 +1,62 @@
 <?php
 
-echo "Hello World!\n";
-echo "Hello World!\n";
-$name = 'Liisu';
-echo 'hello' . $name;
-echo "\n";
-$age = 18;
-echo 'Your name is ' . $name . ' and you are ' . $age . ' years old!';
-echo "\n";
-echo 'Your name is $name and you are $age year sold!!';
-echo "\n";
-echo "Your name is $name and you are $age year sold!!";
-echo "\n";
+$array = array(1, 2, 3);
+var_dump($array);
+$array = [1, 2, 3];
+var_dump($array);
+$array = [
+    1, 
+    'Liisu',
+    true,
+    [1, 2, 3], 
+    3.4, 
+        ];
+var_dump($array);
+$array = [
+    100 => 1,
+    2,
+    'name' => 'Liisu', 
+    'age' => 18,
+    'isFemale' => true,
+    3,
+    4,
+    5,
+        ];
+var_dump($array);
+$array = [
+    1,
+    2,
+    'name' => 'Liisu', 
+    'age' => 18,
+    'isFemale' => true,
+    1534 => 3,
+    4,
+    5,
+        ];
+var_dump($array);
 
-$var = 1;
-var_dump($var);
-$var = 1.4;
-var_dump($var);
-$var = "Hello";
-var_dump($var);
-$var = "Hellõ";
-var_dump($var);
-$var = "😭";
-var_dump($var);
-$var = true;
-var_dump($var);
-$var = false;
-var_dump($var);
-$bar = null;
-var_dump($bar);
+$array = [1, 2, 3];
+var_dump($array[1]);
 
-?>
+$array = [
+    'name' => 'Liisu', 
+    'age' => 18,
+    'isFemale' => true,
+        ];
+var_dump($array['age']);
+
+$array = [
+    [1, 2, 3], 
+    [4, 5, 6], 
+    [7, 8, 9], 
+];
+var_dump($array[2][1]);
+
+$array = [1, 2, 3];
+array_push($array, 4);
+array_push($array, 5, 6, 7);
+$array[] = 8;
+$array[1] = 'something else';
+var_dump($array);
+
+
