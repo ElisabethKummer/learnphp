29938d926a38f4cc1def9b2e0d14dae659146237
Elisabeth Kummer <elisabeth.kummer@tptlive.ee>
2025-09-05 14:15:15 +0300

    public , Private (Visbility)

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 class Box {
     public $height;
-    public $width;
+    private $width;
     public $length;
 
     public function __construct($height=12, $width=12, $length=12) {
@@ -11,12 +11,21 @@ class Box {
         $this->length = $length;
     }
 
+    public function setWidth($width){
+        if(is_numeric($width) && $width > 0){
+            $this->width = $width;
+        } else{
+            throw new Exeption('Width needs to be number and bigger than 0');
+        }
+    }
+
     public function volume(){
         return $this->height * $this->width * $this->length;
     }
 }
 
 $Box1 = new Box(10, 10, 10);
+$Box1->setWidth(20);
 $volume = $Box1->volume();
 var_dump($Box1);
 var_dump($volume);
